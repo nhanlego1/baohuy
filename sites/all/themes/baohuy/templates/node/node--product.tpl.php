ffeedb5da42c53aff2f12d33992db81749690b11
nhan <nhanlego1@gmail.com>
2015-07-29 13:46:51 +0700

    update

--- sites/all/themes/baohuy/templates/node/node--product.tpl.php
+++ sites/all/themes/baohuy/templates/node/node--product.tpl.php
@@ -11,32 +11,17 @@
     <div<?php print $content_attributes; ?>>
         <div class="content-product-top grid-9">
             <div class="image-cloud grid-4">
-                <?php print render($content['field_product_image']); ?>
+                <?php print render($content['field_images']); ?>
             </div>
             <div class="small-info grid-4">
-                <header>
-                    <h2<?php print $title_attributes; ?>><?php print $node->title ?></h2>
-                </header>
                 <?php //krumo($content) ?>
-                <?php print str_replace('SKU','Model',render($content['product:sku']['#markup'])) ?>
-                <?php print render($content['field_brand']) ?>
-                <?php print render($content['field_made_in']) ?>
-                <?php print render($content['field_waranty']) ?>
-                <?php if($node->field_catalog): ?>
-                <div class="field field-name-field-catalog field-type-file field-label-hidden">
-                    <div class="field-items">
-                        <div class="field-item even">
-                            <span class="file">
-
-                                <a href="<?php print file_create_url($node->field_catalog[LANGUAGE_NONE][0]['uri']) ?>" type="text/plain; length=1024">Xem Catalogue</a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <?php endif; ?>
-                <?php print render($content['product:commerce_price']) ?>
+                <?php print str_replace('SKU','Mã sản phẩm',render($content['product:sku']['#markup'])) ?>
+                <?php print render($content['field_warranty']) ?>
+                <?php print render($content['field_size']) ?>
                 <?php print render($content['field_old_price']) ?>
+                <?php print render($content['product:commerce_price']) ?>
                 <?php print render($content['field_product']); ?>
+                <?php print render($content['field_voting']) ?>
                 <?php print render($content['addtoany']) ?>
 
             </div>
@@ -49,6 +34,7 @@
             hide($content['comments']);
             hide($content['links']);
             hide($content['field_catalog']);
+            hide($content['product:sku']);
             print render($content);
             ?></div>
 
