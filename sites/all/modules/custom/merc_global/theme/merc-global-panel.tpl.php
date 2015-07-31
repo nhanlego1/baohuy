fea194b191317d51ca59a600992343bb1889c971
nhan <nhanlego1@gmail.com>
2015-07-31 22:47:18 +0700

    update

--- sites/all/modules/custom/merc_global/theme/merc-global-panel.tpl.php
+++ sites/all/modules/custom/merc_global/theme/merc-global-panel.tpl.php
@@ -2,38 +2,30 @@
     <div class="panel-wrapper">
         <h2>Quản lý sản phẩm</h2>
         <ul>
-            <li class="manage"><a href="/admin/structure/taxonomy/product">Danh mục sản phẩm</a></li>
-            <li class="manage"><a href="/admin/structure/taxonomy/brand">Nhãn hiệu</a></li>
+            <li class="manage"><a href="/admin/structure/taxonomy/category">Danh mục sản phẩm</a></li>
             <li class="manage"><a href="/admin/products">Quản lý sản phẩm </a></li>
             <li class="add"><a href="/node/add/product?destination=admin/products">Thêm sản phẩm </a></li>
 
         </ul>
     </div>
     <div class="panel-wrapper">
-        <h2>Quản lý Order</h2>
+        <h2>Quản lý đơn hàng </h2>
         <ul>
-            <li class="manage"><a href="/admin/commerce/orders">Quản lý Order</a></li>
+            <li class="manage"><a href="/admin/commerce/orders">Quản lý đơn hàng </a></li>
+            <li class="manage"><a href="/admin/commerce/config/payment-methods/manage/commerce_payment_bank_transfer/edit/3">Quản lý thông tin tài khoản ngân hàng  </a></li>
 
         </ul>
     </div>
+
     <div class="panel-wrapper">
-
-        <h2>Quản lý tin tức </h2>
+        <h2>Quản lý bài viết </h2>
         <ul>
-            <li class="manage"><a href="/admin/news">Quản lý tin tức </a></li>
-            <li class="add"><a href="/node/add/article?destination=admin/news">Thêm tin tức </a></li>
+            <li class="manage"><a href="/admin/posts">Quản lý bài viết </a></li>
+            <li class="add"><a href="/node/add/post?destination=admin/posts">Thêm bài viết </a></li>
 
         </ul>
     </div>
-    <div class="panel-wrapper">
-        <h2>Quản lý background </h2>
-        <ul>
-            <li class="manage"><a href="/admin/background">Quản lý background </a></li>
-            <li class="add"><a href="/node/add/background-slideshow?destination=admin/background">Thêm background </a>
-            </li>
 
-        </ul>
-    </div>
     <div class="panel-wrapper">
         <h2>Quản lý Slideshow </h2>
         <ul>
@@ -46,18 +38,10 @@
         <h2>Quản lý Menu</h2>
         <ul>
             <li class="manage"><a href="/admin/structure/menu/manage/main-menu">Quản lý menu chính </a></li>
-            <li class="manage"><a href="/admin/structure/menu/manage/menu-footer">Quản lý menu footer </a></li>
+            <li class="manage"><a href="/admin/structure/menu/manage/menu-footer-top">Quản lý menu footer </a></li>
         </ul>
     </div>
-    <div class="panel-wrapper">
-        <h2>Quản lý nội dung text footer</h2>
-        <ul>
-            <li class="manage"><a
-                    href="/admin/structure/block/manage/block/2/configure?destination=admin/portal/dashboard">Chỉnh sửa
-                    nội dung và link block dưới slideshow </a></li>
 
-        </ul>
-    </div>
     <div class="panel-wrapper">
         <h2>Quản lý thông tin liên hệ </h2>
         <ul>
@@ -69,20 +53,13 @@
         </ul>
     </div>
     <div class="panel-wrapper">
-        <h2>Quản lý Logo</h2>
+        <h2>Quản lý user </h2>
         <ul>
-            <li class="manage"><a href="/admin/appearance/settings/merc?destination=admin/portal/dashboard">Upload new
-                    logo</a></li>
+            <li class="manage"><a href="/admin/people">Quản lý user </a></li>
 
         </ul>
     </div>
-    <div class="panel-wrapper">
-        <h2>Quản lý Metatag</h2>
-        <ul>
-            <li class="manage"><a href="/admin/config/search/metatags">Quản lý Metatag</a></li>
 
-        </ul>
-    </div>
     <div class="panel-wrapper">
         <h2>Đăng xuất </h2>
         <ul>
